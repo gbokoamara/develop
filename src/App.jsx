03ffae65ad794001eb47a16ff73bdee90d0eb07e
GBOKO-git <gboko.amara@gmail.com>
2026-01-13 23:38:53 +0000

    resolve deployment error

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import './App.css'
 import { Route, BrowserRouter as Router , Routes} from "react-router-dom";
 
 import { Home } from './pages/Home'
-import { Header } from './components/layout/header';
-import { Footer } from './components/layout/footer';
+// import { Header } from './components/layout/header';
+// import { Footer } from './components/layout/footer';
 import { SelectionSites } from './pages/SelectionSites';
 import { Telechargement } from './pages/Telechargement';
 import { FormulaireIdee } from './pages/FormulaireIdee';
 import { ProjectDetail } from './pages/ProjectDetail';
-// import { Header } from './components/layout/Header';
-// import { Footer } from './components/layout/Footer';
+import { Header } from './components/layout/Header';
+import { Footer } from './components/layout/Footer';
 
 function App() {
 
